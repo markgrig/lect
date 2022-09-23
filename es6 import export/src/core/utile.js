b813fb5ab947d691437e9bfb2a4fc88741607c12
markgrig <110450201+markgrig@users.noreply.github.com>
2022-09-23 08:44:18 +0300

    23/09/20022

--- es6 import export/src/core/utile.js
+++ es6 import export/src/core/utile.js
@@ -6,7 +6,7 @@ export const getElementDate = ( date ) => {
 }
 
 export const getPriceseDate = ( date1, date2) => {
-    const dateFormate1 = moment( date1 )
+    const dateFormate1 = moment( date1 ) 
     const dateFormate2 =  moment( date2 )
     return moment.preciseDiff(dateFormate1, dateFormate2)
 }
